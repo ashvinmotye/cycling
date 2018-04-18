1d799330b62673c29abdaddacabeac76467094af
ashvintest <motye13@gmail.com>
2018-04-18 22:18:43 +0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,205 @@
+var allRaces = [{
+    "start": "APR-16 2018",
+    "end": "APR-20 2018",
+    "name": "Tour of the Alps",
+    "country": "ITA"
+  },
+  {
+    "start": "APR-17 2018",
+    "end": "APR-22 2018",
+    "name": "Tour of Croatia",
+    "country": "CRO"
+  },
+  {
+    "start": "APR-18 2018",
+    "end": "",
+    "name": "La Flèche Wallonne",
+    "country": "BEL"
+  },
+  {
+    "start": "APR-22 2018",
+    "end": "",
+    "name": "Liège-Bastogne-Liège",
+    "country": "BEL"
+  },
+  {
+    "start": "APR-24 2018",
+    "end": "APR-29 2018",
+    "name": "Tour de Romandie",
+    "country": "SUI"
+  },
+  {
+    "start": "MAY-01 2018",
+    "end": "",
+    "name": "Eschborn-Frankfurt - Rund um den Finanzplatz",
+    "country": "GER"
+  },
+  {
+    "start": "MAY-05 2018",
+    "end": "MAY-27 2018",
+    "name": "Giro d'Italia",
+    "country": "ITA"
+  },
+  {
+    "start": "MAY-08 2018",
+    "end": "MAY-13 2018",
+    "name": "4 Jours de Dunkerque / Grand Prix des Hauts de France",
+    "country": "FRA"
+  },
+  {
+    "start": "MAY-13 2018",
+    "end": "MAY-19 2018",
+    "name": "Amgen Tour of California",
+    "country": "USA"
+  },
+  {
+    "start": "MAY-16 2018",
+    "end": "MAY-20 2018",
+    "name": "Tour of Norway",
+    "country": "NOR"
+  },
+  {
+    "start": "MAY-22 2018",
+    "end": "MAY-24 2018",
+    "name": "Tour des Fjords",
+    "country": "NOR"
+  },
+  {
+    "start": "MAY-23 2018",
+    "end": "MAY-27 2018",
+    "name": "Baloise Belgium Tour",
+    "country": "BEL"
+  },
+  {
+    "start": "MAY-30 2018",
+    "end": "JUN-03 2018",
+    "name": "Skoda-Tour de Luxembourg",
+    "country": "LUX"
+  },
+  {
+    "start": "JUN-03 2018",
+    "end": "JUN-10 2018",
+    "name": "Critérium du Dauphiné",
+    "country": "FRA"
+  },
+  {
+    "start": "JUN-09 2018",
+    "end": "JUN-17 2018",
+    "name": "Tour de Suisse",
+    "country": "SUI"
+  },
+  {
+    "start": "JUL-07 2018",
+    "end": "JUL-14 2018",
+    "name": "Int. Österreich-Rundfahrt-Tour of Austria",
+    "country": "AUT"
+  },
+  {
+    "start": "JUL-07 2018",
+    "end": "JUL-29 2018",
+    "name": "Tour de France",
+    "country": "FRA"
+  },
+  {
+    "start": "JUL-28 2018",
+    "end": "AUG-01 2018",
+    "name": "VOO-Tour de Wallonie",
+    "country": "BEL"
+  },
+  {
+    "start": "JUL-29 2018",
+    "end": "",
+    "name": "Prudential RideLondon-Surrey Classic",
+    "country": "GBR"
+  },
+  {
+    "start": "AUG-01 2018",
+    "end": "AUG-05 2018",
+    "name": "PostNord Danmark Rundt - Tour of Denmark",
+    "country": "DEN"
+  },
+  {
+    "start": "AUG-04 2018",
+    "end": "",
+    "name": "Clasica Ciclista San Sebastian",
+    "country": "ESP"
+  },
+  {
+    "start": "AUG-04 2018",
+    "end": "AUG-10 2018",
+    "name": "Tour de Pologne",
+    "country": "POL"
+  },
+  {
+    "start": "AUG-07 2018",
+    "end": "AUG-11 2018",
+    "name": "Vuelta a Burgos",
+    "country": "ESP"
+  },
+  {
+    "start": "AUG-08 2018",
+    "end": "AUG-12 2018",
+    "name": "Arctic Race of Norway",
+    "country": "NOR"
+  },
+  {
+    "start": "AUG-13 2018",
+    "end": "AUG-19 2018",
+    "name": "Binck Bank Tour",
+    "country": "---"
+  },
+  {
+    "start": "AUG-19 2018",
+    "end": "",
+    "name": "EuroEyes Cyclassics Hamburg",
+    "country": "GER"
+  },
+  {
+    "start": "AUG-25 2018",
+    "end": "SEP-16 2018",
+    "name": "La Vuelta ciclista a España",
+    "country": "ESP"
+  },
+  {
+    "start": "AUG-26 2018",
+    "end": "",
+    "name": "Bretagne Classic - Ouest-France",
+    "country": "FRA"
+  },
+  {
+    "start": "SEP-02 2018",
+    "end": "SEP-09 2018",
+    "name": "Ovo Energy Tour of Britain",
+    "country": "GBR"
+  },
+  {
+    "start": "SEP-07 2018",
+    "end": "",
+    "name": "Grand Prix Cycliste de Québec",
+    "country": "CAN"
+  },
+  {
+    "start": "SEP-09 2018",
+    "end": "",
+    "name": "Grand Prix Cycliste de Montréal",
+    "country": "CAN"
+  },
+  {
+    "start": "OCT-09 2018",
+    "end": "OCT-14 2018",
+    "name": "Presidential Cycling Tour of Turkey",
+    "country": "TUR"
+  },
+  {
+    "start": "OCT-13 2018",
+    "end": "",
+    "name": "Il Lombardia",
+    "country": "ITA"
+  },
+  {
+    "start": "OCT-16 2018",
+    "end": "OCT-21 2018",
+    "name": "Gree-Tour of Guangxi",
+    "country": "CHN"
+  }
+];
---
+++ main.js
@@ -0,0 +1,98 @@
+var content = document.querySelector('#content');
+
+window.onload = populate();
+
+function populate() {
+  for(var i=0; i<allRaces.length; i++) {
+
+    var raceContainer = this.document.createElement('div');
+    raceContainer.className = 'race';
+
+    var raceNameP = this.document.createElement('p');
+    raceNameP.classList.add('race-item');
+    raceNameP.classList.add('race-name');
+    raceNameP.innerHTML = allRaces[i].name;
+
+    var raceDateP = this.document.createElement('p');
+    raceDateP.classList.add('race-item');
+    raceDateP.classList.add('race-date');
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var raceStartDate = new Date(allRaces[i].start).toLocaleDateString("en-US", options);
+    var today = new Date().toLocaleDateString("en-US", options);
+
+    if(raceStartDate === today) {
+      raceDateP.innerHTML = 'today';
+      raceContainer.classList.add('ongoing');
+    } 
+    
+    else {
+      raceDateP.innerHTML = raceStartDate.replace(', 2018', '');
+    }
+
+    var raceCountdownP = this.document.createElement('p');
+    raceCountdownP.classList.add('race-item');
+    raceCountdownP.classList.add('race-countdown');
+    
+    if(allRaces[i].end !== '') {
+      var raceEnd = new Date(allRaces[i].end);
+      var raceStart = new Date(allRaces[i].start);
+      var current = new Date();
+
+      if(current <= raceEnd && current >= raceStart) {
+        raceContainer.classList.add('ongoing');
+        raceDateP.innerHTML = 'ongoing';
+
+        var stage = (current - raceStart) / 1000 / 60 / 60 / 24;
+        var remaining = (raceEnd - current) / 1000 / 60 / 60 / 24;
+        remaining = Math.floor(remaining)+1;
+        stage = Math.floor(stage) + 1;
+
+        raceCountdownP.innerHTML = 'stage ' + stage + ' ('+remaining+' left)';
+      }
+
+      else if(raceEnd < current) {
+        raceDateP.innerHTML = 'complete';
+        raceCountdownP.innerHTML = 'stage race';
+        raceContainer.classList.add('complete');
+      }
+
+      else if(current < raceStart) {
+        var days = (raceStart - current) / 1000 / 60 / 60 / 24;
+        days = Math.floor(days);
+        raceCountdownP.innerHTML = days + ' days';
+      }
+    }
+
+    else {
+      var oneDayRaceStart = allRaces[i].start+' 23:59:59';
+      oneDayRaceStart = new Date(oneDayRaceStart);
+      var oneDayCurrent = new Date();
+
+      if (oneDayRaceStart < oneDayCurrent) {
+        raceDateP.innerHTML = 'complete';
+        raceContainer.classList.add('complete');
+        raceCountdownP.innerHTML = 'one day race';
+      }
+
+      else if (oneDayRaceStart > oneDayCurrent) {
+        var daysToStart = (oneDayRaceStart - oneDayCurrent) / 1000 / 60 / 60 / 24;
+        daysToStart = Math.floor(daysToStart);
+
+        if(daysToStart !== 0) {
+          raceCountdownP.innerHTML = daysToStart + ' days';
+        }
+
+        else {
+          raceCountdownP.innerHTML = 'one day race';
+        }
+      }
+    }
+
+    raceContainer.appendChild(raceNameP);
+    raceContainer.appendChild(raceDateP);
+    raceContainer.appendChild(raceCountdownP);
+
+    content.appendChild(raceContainer);
+
+  }
+}
